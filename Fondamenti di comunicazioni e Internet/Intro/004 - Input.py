6411a7eb45c89c4a8106cb0ed5aa7553e26421ae
Riccardo Vescio <101594872+Richard-VR0@users.noreply.github.com>
2024-03-06 13:52:42 +0100

    Delete Fondamenti di comunicazioni e Internet/Intro directory

--- Fondamenti di comunicazioni e Internet/Intro/004 - Input.py
+++
@@ -1,7 +0,0 @@
-x = input("Clicca qualche tasto: ")
-
-print(type(x))          #Il tipo è sempre str se non facciamo casting sull'input
-
-y = int(input("Inserisci un numero: "))
-
-print(type(y))
--- Fondamenti di comunicazioni e Internet/Intro/006 - Selezione.py
+++
@@ -1,8 +0,0 @@
-age = int(input("Inserisci un l'èta: "))
-
-if age > 18:
-    print("Maggiorenne")
-elif age == 18:                                 #elif corrisponde alla struttura "else if"
-    print("Appena diventato maggiorenne")
-else:
-    print("Minorenne")
